0640c82279b378e38185780c20a3acb7eb2a86d8
skull 47 <vsad258369@gmail.com>
2026-04-10 15:40:45 +0530

    Feat: Add case-insensitive string transformation utility

--- anti_ransomware.cpp
+++ anti_ransomware.cpp
@@ -13,6 +13,12 @@ using namespace std;
 
 const double ENTROPY_THRESHOLD = 7.5;
 
+string to_lower(const string& str) {
+    string lower_str = str;
+    transform(lower_str.begin(), lower_str.end(), lower_str.begin(), ::tolower);
+    return lower_str;
+}
+
 int main() {
     cout << "Anti-Ransomware Framework Initialized.\n";
     return 0;
